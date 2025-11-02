6779d8bca1c9b98ce642f380bcefad8343c3841f
dembelecomvitiligo <santossjoao853@gmail.com>
2025-11-02 00:34:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*Vou fazer a logica da tabuada aqui*/
+
+document.getElementById('bnt').onclick = function () {
+    let numero = parseInt(document.getElementById('numero').value);
+    /*Nessa function vou pegar o valor digitado no input e fazer a tabuada*/
+   
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = ''; // Limpa o resultado anterior
+
+    for (let i = 1; i <= 10; i++) {
+        let produto = numero * i;
+        resultado.innerHTML += `${numero} x ${i} = ${produto}<br>`;
+    }
+}
